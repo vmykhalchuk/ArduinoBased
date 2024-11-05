4ed4454b6c68d3c54b2b453209b764bcc825b527
Volodymyr Mykhalchuk <vmykhalchuk@vertafore.com>
2024-11-05 19:41:08 +0200

    Lights Controller - initial prototype  - need to test it first directly, then use solution#2 then fix  solution#2 to use inertia mechanism

--- projects/pinky_cloud_house/lights_controller/lights_controller.ino
+++ projects/pinky_cloud_house/lights_controller/lights_controller.ino
@@ -22,6 +22,8 @@ const uint8_t MAP_COUNT = 6;
 const uint8_t pinOutputs[MAP_COUNT] = {4,5,6,7,8,9};
 
 uint8_t states = 0;
+
+// for solution #1
 uint8_t inputChangeRequested = 0;
 unsigned long inputChangeLastAttemptTimeMs[MAP_COUNT];
 
@@ -34,6 +36,7 @@ void setup() {
     inputChangeLastAttemptTimeMs[i] = 0;
     pinMode(pinOutputs[i], OUTPUT);
   }
+
 }
 
 void loop() {
@@ -51,8 +54,66 @@ void loop_test() {
   }
 }
 
+void loop_main_solution2() {
+  uint8_t error = 0;
+
+  const uint8_t statisticsSize = 100; // by changing this value we modify system reactio time (reducing value increases reaction time and vice versa)
+  uint8_t statisticsPointer = 0; // where pointer is currently waiting for data to come in
+  uint8_t statistics[statisticsSize]; for (uint8_t i = 0; i < statisticsSize; i++) statistics[i] = 0;
+  uint8_t statisticsHighLevelCount[MAP_COUNT]; for (uint8_t i = 0; i < MAP_COUNT; i++) statisticsHighLevelCount[i] = 0;
+  
+  const uint16_t inertiaTicks = 1000;//for how many ticks output state will not change to new value
+  uint16_t outputLastTimeUpdatedTicksAgo[MAP_COUNT]; for (uint8_t i = 0; i < MAP_COUNT; i++) outputLastTimeUpdatedTicksAgo[i] = inertiaTicks;
+  const uint8_t highLevelThresholdPercentage = 70;
+  const uint16_t t = ((uint16_t) statisticsSize * 100) / highLevelThresholdPercentage;
+  const uint8_t highLevelThresholdCount = t;
+
+  uint8_t masks[8]; uint8_t invMasks[8]; for (uint8_t i = 0; i < 8; i++) { masks[i] = 2 << i; invMasks[i] = (!masks[i]); }
+  
+  while (true) {
+    uint8_t data = PINC & B00111111;
+
+    // update statistics & statisticsHighLevelCount with new observation (data)
+    for (uint8_t i = 0; i < MAP_COUNT; i++) {
+      uint8_t mask = 2 << i;
+      if (statistics[statisticsPointer] & mask != 0) {
+        if (statisticsHighLevelCount[i] == 0) error = 0x01;
+        else statisticsHighLevelCount[i]--;
+      }
+      if (data & mask != 0) {
+        if (statisticsHighLevelCount[i] > statisticsSize) error = 0x02;
+        else statisticsHighLevelCount[i]++;
+      }
+    }
+    statistics[statisticsPointer] = data;
+
+    // increase tick counters for every value changed
+    for (uint8_t i = 0; i < MAP_COUNT; i++) if (outputLastTimeUpdatedTicksAgo[i] < inertiaTicks) outputLastTimeUpdatedTicksAgo[i]++;
+    
+    // update outputs to represent new stats
+    for (uint8_t i = 0; i < MAP_COUNT; i++) {
+      uint8_t mask = masks[i];
+      uint8_t invMask = invMasks[i];
+      uint8_t hlc = statisticsHighLevelCount[i];
+      uint8_t newPinState = hlc >= highLevelThresholdCount ? mask : 0;
+      uint8_t oldPinState = states & mask;
+      uint16_t t = 0; // inertiaTicks // FIXME for testing we have it zero, when tested - change it back
+      if (newPinState != oldPinState && outputLastTimeUpdatedTicksAgo[i] >= t) {
+        if (newPinState == 0) {
+          digitalWrite(pinOutputs[i], LOW);
+          states = states & invMask;
+        } else {
+          digitalWrite(pinOutputs[i], HIGH);
+          states = states | mask;
+        }
+        outputLastTimeUpdatedTicksAgo[i] = 0;
+      }
+    }
+  }
+}
+
 uint8_t previousData = 0;
-void loop_main() {
+void loop_main_solution1() {
   uint8_t data = PINC & B00111111;
   if (data != previousData) {
     handleNewData(data, data ^ previousData);
