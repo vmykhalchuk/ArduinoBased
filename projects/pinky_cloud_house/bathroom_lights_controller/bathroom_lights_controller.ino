8d46271ffb11d129c74284ea97fc0e0296e05abb
unknown <vmykhalchuk@gmail.com>
2025-01-24 01:54:06 +0200

    [lighting] first stable version

--- projects/pinky_cloud_house/bathroom_lights_controller/bathroom_lights_controller.ino
+++ projects/pinky_cloud_house/bathroom_lights_controller/bathroom_lights_controller.ino
@@ -4,10 +4,12 @@ const uint8_t pinLed1 = 5;
 const uint8_t pinLed2 = 6;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(pinSwitch, INPUT);
   digitalWrite(pinSwitch, HIGH);
   pinMode(pinLed1, OUTPUT);
   pinMode(pinLed2, OUTPUT);
+  Serial.println("Started!");
 }
 
 /*
@@ -33,6 +35,8 @@ void loop() {
 }
 */
 
+const uint8_t minPwmState = 20;
+bool ledStateIsOn = false;
 uint8_t ledPwmState = 230;
 bool ascendLedPwm = false;
 
@@ -43,10 +47,19 @@ bool switchState = HIGH;
 // switch click/longclick part
 const uint16_t shortClickMaxTimeMs = 500;
 const uint16_t longClickMinTimeMs = 1000;
-const uint16_t longClickStepMs = 8;
+const uint16_t longClickStepMs = 10;
 bool clickStarted = false;
 uint16_t clickStartMs = 0;
 uint8_t initialLedPwmState = 0;
+
+void fadeOut(uint8_t pin, uint8_t currentValue) {
+  for (uint8_t i = currentValue; i>=1; i--) {
+    delay(3);
+    analogWrite(pin,i);
+  }
+  delay(3);
+  analogWrite(pin,0);
+}
 
 void loop() {
   bool switchStateChanged = false;
@@ -76,30 +89,41 @@ void loop() {
     if ((_ms - clickStartMs) < shortClickMaxTimeMs) {
       // short click (toggle switch)
       _ledStateToggle = true;
+
+      ledStateIsOn = !ledStateIsOn;
+      if (ledStateIsOn) {
+        analogWrite(pinLed1, ledPwmState);
+        analogWrite(pinLed2, ledPwmState);
+      } else {
+        fadeOut(pinLed1, ledPwmState);//analogWrite(pinLed1, 0);
+        fadeOut(pinLed2, ledPwmState);//analogWrite(pinLed2, 0);
+      }
     } else if ((_ms - clickStartMs) >= longClickMinTimeMs) {
+      //Serial.println();
+      //Serial.println("TOGGLED PWM ASCENDING!");
       ascendLedPwm = !ascendLedPwm;
     }
   
   } else if (clickStarted && (_ms - clickStartMs) >= longClickMinTimeMs) {
 
-    uint16_t steps = (_ms - clickStartMs) / longClickStepMs;
+    uint16_t steps = (_ms - clickStartMs - longClickMinTimeMs) / longClickStepMs;
     if (steps == 0) {
       initialLedPwmState = ledPwmState;
-    } else {
-      if (steps < 300) {
-        uint16_t r = 10000 + initialLedPwmState;
-        if (ascendLedPwm) r += steps;
-        else r -= steps;
+    } else if (steps < 300) {
+      uint16_t r = 10000 + initialLedPwmState - minPwmState;
+      if (ascendLedPwm) r += steps;
+      else r -= steps;
 
-        uint8_t rr;
-        if (r < 10000) rr = 0;
-        else if (r > 10255) rr = 255;
-        else rr = (r - 10000);
+      uint8_t rr;
+      if (r < 10000) rr = 0;
+      else if (r > (10255-minPwmState)) rr = 255-minPwmState;
+      else rr = (r - 10000);
 
-        ledPwmState = rr;
-        analogWrite(pinLed1, ledPwmState);
-        analogWrite(pinLed2, ledPwmState);
-      }
+      //Serial.print(rr,HEX); Serial.print(" ");
+      ledPwmState = rr + minPwmState;
+      analogWrite(pinLed1, ledPwmState);
+      analogWrite(pinLed2, ledPwmState);
+      ledStateIsOn = true;
     }
   }
 }
