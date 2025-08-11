5ee94d3b9e822a24e0d1aef83eb6d10c9e8ad152
Volodymyr Mykhalchuk <vmykhalchuk@vertafore.com>
2025-08-11 17:48:11 +0300

    .

--- projects/moto/temperature_sensor/temperature_sensor.ino
+++ projects/moto/temperature_sensor/temperature_sensor.ino
@@ -15,7 +15,31 @@ void setup() {
 
 void loop() {
   sensors.requestTemperatures();
-  Serial.print("Celcius temperature: ");
-  Serial.println(sensors.getTempCByIndex(0));
+  float tempC = sensors.getTempCByIndex(0);
+  
+  // Check if reading was successful
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+
+    int d100 = (int)(tempC / 100) % 10;
+    int d10 = (int)(tempC / 10) % 10;
+    int d1 = (int)tempC % 10;
+    int d0 = (int)(tempC * 10) % 10;
+    Serial.print("[");
+    Serial.print(d100);
+    Serial.print(",");
+    Serial.print(d10);
+    Serial.print(",");
+    Serial.print(d10);
+    Serial.print(",");
+    Serial.print(d0);
+    Serial.println("]");
+  }
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
   delay(1000);
 }
