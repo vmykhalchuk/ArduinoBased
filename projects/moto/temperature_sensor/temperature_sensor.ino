8e7a5189f472a0b048ae18e887a3a25640350660
Volodymyr Mykhalchuk <vmykhalchuk@vertafore.com>
2025-08-11 19:13:37 +0300

    .

--- projects/moto/temperature_sensor/temperature_sensor.ino
+++ projects/moto/temperature_sensor/temperature_sensor.ino
@@ -9,13 +9,6 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 void setup() {
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  
   Serial.begin(9600);
   sensors.begin();
 }
@@ -54,22 +47,72 @@ void loop() {
 }
 
 
+// 1->D5;2->D6;3->D7;4->B0;5->B1;6->B2
+
 
 void displayDigits(int c) {
-  c = c % 3; //12;
+  c = c % 6; //12;
   if (c == 0) {
-    digitalWrite(4+1, LOW); digitalWrite(4+2, HIGH);
-    digitalWrite(4+3, HIGH); digitalWrite(4+5, LOW);
-    
-    digitalWrite(4+4, LOW); digitalWrite(4+6, LOW);
+    setDPin(1,LOW); setDPin(2,HIGH);
+    clearDPin(3); clearDPin(5);//setDPin(3,LOW); setDPin(5,HIGH);
+    clearDPin(4); clearDPin(6);
   } else if (c == 1) {
-    digitalWrite(4+1, LOW); digitalWrite(4+3, HIGH);
-    digitalWrite(4+2, LOW); digitalWrite(4+6, HIGH);
-
-    digitalWrite(4+4, LOW); digitalWrite(4+6, LOW);
+    setDPin(1,LOW); setDPin(3,HIGH);
+    clearDPin(2); clearDPin(6);//setDPin(2,LOW); setDPin(6,HIGH);
+    clearDPin(4); clearDPin(5);
   } else if (c == 2) {
-    digitalWrite(4+1, LOW); digitalWrite(4+2, LOW);
-    digitalWrite(4+3, LOW); digitalWrite(4+4, LOW);
-    digitalWrite(4+5, LOW); digitalWrite(4+6, LOW);
+    clearDPin(1); clearDPin(2);
+    clearDPin(3); clearDPin(4);
+    clearDPin(5); clearDPin(6);
+    
+  } else if (c == 3) {
+    setDPin(1,LOW); setDPin(2,HIGH);
+    setDPin(3,LOW); setDPin(5,HIGH);
+    clearDPin(4); clearDPin(6);
+  } else if (c == 4) {
+    setDPin(1,LOW); setDPin(3,HIGH);
+    setDPin(2,LOW); setDPin(6,HIGH);
+    clearDPin(4); clearDPin(5);
+  } else if (c == 5) {
+    clearDPin(1); clearDPin(2);
+    clearDPin(3); clearDPin(4);
+    clearDPin(5); clearDPin(6);
+  }
+}
+
+void setDPin(uint8_t p, uint8_t setHigh) {
+  if (p == 1) {
+    DDRD = DDRD | B00100000;
+    if (setHigh == HIGH) PORTD = PORTD | B00100000; else PORTD = PORTD & B11011111;
+  } else if (p == 2) {
+    DDRD = DDRD | B01000000;
+    if (setHigh == HIGH) PORTD = PORTD | B01000000; else PORTD = PORTD & B10111111;
+  } else if (p == 3) {
+    DDRD = DDRD | B10000000;
+    if (setHigh == HIGH) PORTD = PORTD | B10000000; else PORTD = PORTD & B01111111;
+  } else if (p == 4) {
+    DDRB = DDRB | B00000001;
+    if (setHigh == HIGH) PORTB = PORTB | B00000001; else PORTB = PORTB & B11111110;
+  } else if (p == 5) {
+    DDRB = DDRB | B00000010;
+    if (setHigh == HIGH) PORTB = PORTB | B00000010; else PORTB = PORTB & B11111101;
+  } else if (p == 6) {
+    DDRB = DDRB | B00000100;
+    if (setHigh == HIGH) PORTB = PORTB | B00000100; else PORTB = PORTB & B11111011;
+  }
+}
+void clearDPin(uint8_t p) {
+  if (p == 1) {
+    DDRD = DDRD & B11011111;
+  } else if (p == 2) {
+    DDRD = DDRD & B10111111;
+  } else if (p == 3) {
+    DDRD = DDRD & B01111111;
+  } else if (p == 4) {
+    DDRB = DDRB & B11111110;
+  } else if (p == 5) {
+    DDRB = DDRB & B11111101;
+  } else if (p == 6) {
+    DDRB = DDRB & B11111011;
   }
 }
