0479cf993378754a421cb2be016e02158fef093a
Volodymyr Mykhalchuk <vmykhalchuk@gmail.com>
2025-12-28 22:15:47 +0200

    .

--- test_scketches/play_tune/play_tune.ino
+++ test_scketches/play_tune/play_tune.ino
@@ -1,41 +1,24 @@
-/*
-
-  Melody
-
-  Plays a melody
-
-  circuit:
-
-  - 8 ohm speaker on digital pin 8
-
-  created 21 Jan 2010
-
-  modified 30 Aug 2011
-
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/Tone
-
-*/
-
 #include "pitches.h"
 
 // notes in the melody:
 int melody[] = {
-
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  NOTE_A1, NOTE_B1, NOTE_C2, NOTE_D3, NOTE_E4, 0, NOTE_F5, NOTE_G6
+  //NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-
-  4, 8, 8, 4, 4, 4, 4, 4
+  8, 8, 8, 8, 8, 8, 8, 8
+  //4, 8, 8, 4, 4, 4, 4, 4
 };
+
+const int pinZoomer = 4;
 
 void setup() {
 
+}
+
+void loop() {
   // iterate over the notes of the melody:
 
   for (int thisNote = 0; thisNote < 8; thisNote++) {
@@ -46,7 +29,7 @@ void setup() {
 
     int noteDuration = 1000 / noteDurations[thisNote];
 
-    tone(8, melody[thisNote], noteDuration);
+    tone(pinZoomer, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
 
@@ -58,12 +41,10 @@ void setup() {
 
     // stop the tone playing:
 
-    noTone(8);
+    noTone(pinZoomer);
 
   }
-}
 
-void loop() {
+  delay(3000);
 
-  // no need to repeat the melody.
 }
