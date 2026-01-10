33f762d4343341caf9bdf3cc2b065d30cfcdeff9
Volodymyr Mykhalchuk <vmykhalchuk@gmail.com>
2026-01-10 17:46:12 +0200

    .

--- test_scketches/sounds/ToneByRogueCode/RTTL_and_sensor/RTTL_and_sensor.ino
+++ test_scketches/sounds/ToneByRogueCode/RTTL_and_sensor/RTTL_and_sensor.ino
@@ -10,18 +10,20 @@
 #include "pitches.h"
 
 const int buzzerPin = 4;
+const int vibroPin = A4;
 const int moveSensorPin = A7; // < 200 - not pressed
 const int buttonPin = A5; // LOW - not pressed - HIGH - pressed
-const unsigned long PLAY_SHORT_TUNE_BEFORE_TIMEPASSED_MS = 60000;
-const unsigned long DELAY_BETWEEN_SONGS_MIN_MS = 1500;
+const unsigned int PLAY_SHORT_TUNE_BEFORE_TIMEPASSED_MS = 60000;
+const unsigned int DELAY_BETWEEN_SONGS_MIN_MS = 1500;
+const unsigned int VIBRO_CONFIRM_MS = 300;
 
 #define OCTAVE_OFFSET 0
 
 int notes[] = { 0,
-NOTE_C4, NOTE_CS4, NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4, NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4,
-NOTE_C5, NOTE_CS5, NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5,
-NOTE_C6, NOTE_CS6, NOTE_D6, NOTE_DS6, NOTE_E6, NOTE_F6, NOTE_FS6, NOTE_G6, NOTE_GS6, NOTE_A6, NOTE_AS6, NOTE_B6,
-NOTE_C7, NOTE_CS7, NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7
+  NOTE_C4, NOTE_CS4, NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4, NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4,
+  NOTE_C5, NOTE_CS5, NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5,
+  NOTE_C6, NOTE_CS6, NOTE_D6, NOTE_DS6, NOTE_E6, NOTE_F6, NOTE_FS6, NOTE_G6, NOTE_GS6, NOTE_A6, NOTE_AS6, NOTE_B6,
+  NOTE_C7, NOTE_CS7, NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7
 };
 
 char buffer[1000];
@@ -65,6 +67,7 @@ void setup(void)
 {
   Serial.begin(9600);
   pinMode(buzzerPin, OUTPUT);
+  pinMode(vibroPin, OUTPUT);
   delay(2000);
 }
 
@@ -280,20 +283,32 @@ void loop(void)
   lastTimeTriggeredMs = currentTimeMs;
 
   strcpy_P(buffer, melodies[melodyNo%melodiesCount]);
+  // res becomes true when button pressed
   bool res = play_rtttl(buffer, (playShort || recentlyPlayed) ? 2200 : 0);
 
   if (res) {
+    bool vibroOn = true;
+    digitalWrite(vibroPin, HIGH);
+    
     unsigned long startMs = millis();
     lastTimeTriggeredMs -= PLAY_SHORT_TUNE_BEFORE_TIMEPASSED_MS + 1;
     bool longPress = false;
     while (digitalRead(buttonPin) == HIGH) {
-      if (!longPress && (millis() - startMs > LONG_PRESS__DELAY_MS)) {
+      unsigned long passedMs = millis() - startMs;
+      if (vibroOn && (passedMs > VIBRO_CONFIRM_MS)) {
+        digitalWrite(vibroPin, LOW);
+        vibroOn = false;
+      }
+      if (!longPress && (passedMs > LONG_PRESS__DELAY_MS)) {
         tone(buzzerPin, LONG_PRESS__CONFIRM_TONE);
         startMs = millis();
         longPress = true;
       }
-      if (longPress && (millis() - startMs >= LONG_PRESS__CONFIRM_TIME_MS)) noTone(buzzerPin);
+      if (longPress && (passedMs >= LONG_PRESS__CONFIRM_TIME_MS)) {
+        noTone(buzzerPin);
+      }
     } // wait till it drops to LOW
+    if (vibroOn) digitalWrite(vibroPin, LOW);
 
     if (longPress) {
       unsigned long diffMs = millis() - startMs;
